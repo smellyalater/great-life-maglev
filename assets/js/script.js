0597d773c9fd35c3e4462b276ba777dda168ae06
Andy Copleman <acopleman@gmail.com>
2016-02-28 13:18:03 -0500

    fixed scrollto feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-jQuery.extend(jQuery.easing,{
-    easeInOutExpo: function(x, t, b, c, d){
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    }
-});
 
 
 
 
 $(document).ready(function() {
 
-      
 
-    //click elements to expand/collapse content
+    
+
+
+
+
+    //
+    // BEGIN click elements to expand/collapse content
+    //
+
     $('.js-more-expand').click(function() {
         var $moreButton = $(this);
         $(this).parent().next().slideToggle(400);
@@ -22,40 +21,49 @@ $(document).ready(function() {
     });
         // flipCaret();
 
+
+
+
+    //
     //Expand email form on click
+    //
+
     $('.js-email-form-expander').click(function() {
         $('.email-form').slideToggle(400);
     });
 
-    // END click elements to expand/collapse content
 
+    //
+    // END click elements to expand/collapse content
+    //
 
 
     //Click links in nav to scroll you down to section
-    $('a[href=#]').click(function(e){
-        e.preventDefault();
-        $('nav').removeClass('visible');
-        $('html,body').stop().animate({scrollTop: $('.'+$(this).data('scrollto')).offset().top-65 }, 700, 'easeInOutExpo', function(){});
-    });
-
-    $('.toggle-menu').click(function(){
-        $('nav').toggleClass('visible');
-    });
-
-    if($(window).width() < 800){
-        $('.pricing > div > div:nth-of-type(3)').insertAfter($('.pricing > div > div:nth-of-type(1)'));
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 900);
+        return false;
+      }
     }
+  });
+});
 
     // END Click links in nav to scroll you down to section
 
     //Youtube Junk
       $(".youtube").each(function() {
     // Set the BG image from the youtube ID
-    $(this).css('background-image', 'url(https://i.ytimg.com/vi_webp/' + this.id + '/mqdefault.webp)');
+    $(this).css('background-image', 'url(//i.ytimg.com/vi_webp/' + this.id + '/mqdefault.webp)');
     // Click the video div
     $(document).delegate('#' + this.id, 'click', function() {
       // Build embed URL
-      var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+      var iframe_url = "//www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
       // Grab extra parameters set on div
       if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
       // Build iframe tag
