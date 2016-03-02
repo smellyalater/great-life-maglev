a55bc9599b5429ca632e43e83e0a78dcee2eab62
Andy Copleman <acopleman@gmail.com>
2016-03-01 23:17:08 -0500

    created email thank you page, added headlines, adjusted spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,9 @@
 
 $(document).ready(function() {
 
-
-    
-
-
-
-
-    //
-    // BEGIN click elements to expand/collapse content
-    //
+//
+// BEGIN click elements to expand/collapse content
+//
 
     $('.js-more-expand').click(function() {
         var $moreButton = $(this);
@@ -21,24 +15,26 @@ $(document).ready(function() {
     });
         // flipCaret();
 
-
-
-
-    //
-    //Expand email form on click
-    //
+//
+//Expand email form on click
+//
 
     $('.js-email-form-expander').click(function() {
         $('.email-form').slideToggle(400);
     });
 
 
-    //
-    // END click elements to expand/collapse content
-    //
+//
+// END click elements to expand/collapse content
+//
 
 
-    //Click links in nav to scroll you down to section
+
+
+//
+// BEGIN Click links in nav to scroll you down to section
+//
+
 $(function() {
 
   $('a[href*=#]:not([href=#])').click(function() {
@@ -56,24 +52,10 @@ $(function() {
   });
 });
 
-    // END Click links in nav to scroll you down to section
 
-    //Youtube Junk
-      $(".youtube").each(function() {
-    // Set the BG image from the youtube ID
-    $(this).css('background-image', 'url(//i.ytimg.com/vi_webp/' + this.id + '/mqdefault.webp)');
-    // Click the video div
-    $(document).delegate('#' + this.id, 'click', function() {
-      // Build embed URL
-      var iframe_url = "//www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
-      // Grab extra parameters set on div
-      if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
-      // Build iframe tag
-      var iframe = $('<iframe/>', {'allowfullscreen':'allowfullscreen', 'frameborder': '0', 'src': iframe_url})
-      // Replace the YouTube thumbnail with YouTube HTML5 Player
-      $(this).append(iframe);
-    });// /click
-  }); // /each video
+//
+// END Click links in nav to scroll you down to section
+//
 
 
 
